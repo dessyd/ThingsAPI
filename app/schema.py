17d8516a9e9df1a772eab77c4ae239fc1a7bfd5e
Dominique Dessy <dominique.dessy@gmail.com>
2021-12-02 09:39:06 +0100

    adding sensors

--- app/schema.py
+++ app/schema.py
@@ -1,32 +1,42 @@
+from re import T
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.expression import null, text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
-
-class Post(Base):
-    __tablename__ = "posts"
-
+# A thing, the physical object is made of 
+# a Board, 
+# zero or more Sensor(s) 
+# zero or more Actuator(s)
+# and pysicallly located at Location
+class Thing(Base):
+    __tablename__ = "things"
     id = Column(Integer, primary_key=True, nullable=False)
-    title = Column(String, nullable=False)
-    content = Column(String, nullable=False)
-    published = Column(Boolean, server_default='TRUE', nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-    owner = relationship("User")
+    boardid = Column(Integer,nullable=False)
+    locationid = Column(Integer,nullable=False)
 
-class User(Base):
-    __tablename__ = "users"
+# A Board is the generic description
+class Board(Base):
+    __tablename__ = "boards"
+    id = Column(Integer, primary_key=True, nullable=False)   
+    name = Column(String, nullable=True)
 
+class Location(Base):
+    __tablename__ = "locations"
+    id = Column(Integer, primary_key=True, nullable=False)  
+    name = Column(String, nullable=True)
+    lat = Column(Float, nullable=False)
+    lon = Column(Float, nullable=False)
+    
+class Sensor(Base):
+    __tablename__ = "sensors"
     id = Column(Integer, primary_key=True, nullable=False)
-    email = Column(String, nullable=False, unique=True)
-    password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    boardid = Column(Integer, nullable=False)
+    enabled = Column(Boolean, server_default='TRUE', nullable=False)
+    type = Column(Integer, nullable=False)
 
-
-class Vote(Base):
-    __tablename__ = "votes"
-
-    user_id = Column(Integer, ForeignKey("users.id",ondelete="CASCADE"), nullable=False, primary_key=True)
-    post_id = Column(Integer, ForeignKey("posts.id",ondelete="CASCADE"), nullable=False, primary_key=True)
+class SensorType(Base):
+    __tablename__ = "sensortypes"
+    id = Column(Integer, primary_key=True, nullable=False)
+    type = Column(String, nullable=False)
